42b98a824a1039cf1c6c1da9585d1af7d507e6c4
margarita2015 <mcorreo200@yahoo.es>
2015-07-02 17:04:33 +0200

    Fase 5 y modificaciones
    Modulo 6- Proyecto Quiz- P2P Obligatorio

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,10 @@ router.get('/', function(req, res) {
 router.get('/quizes/question', quizController.question); //introducido fase primera pregunta
 router.get('/quizes/answer',   quizController.answer);  //introducido fase primera pregunta
 
+router.get('/author', function(req, res) {  //introducido fase quiz-5-modificaciones finales, lineas 14-17
+  //res.render('author', {layout:false} );
+  res.render('author', {} );
+});
+
+
 module.exports = router;
